e435682e90d71a86d8057ac3f2784607d5634927
crtusr <lucasgasperi03@gmail.com>
2026-05-23 16:38:39 -0300

    Arreglando merge conflicts

--- managerpersonaje.h
+++ managerpersonaje.h
@@ -2,22 +2,6 @@
 #define MANAGERPERSONAJE_H_INCLUDED
 #include <SFML/Graphics.hpp>
 #include "personaje.h"
-<<<<<<< HEAD
-
-class managerpersonaje 
-{
-  private:
-
-
-  public:
-
-    void moverpersonaje(personaje &pers);
-    void mostrarpersonaje(personaje pers,sf::RenderWindow& window);
-
-};
-
-#endif // MANAGERPERSONAJE_H_INCLUDED
-=======
 
 
 
@@ -34,4 +18,3 @@ void Asignarpersonajes(personaje& pers);
 
 };
 #endif // MANAGERPERSONAJE_H_INCLUDED
->>>>>>> refs/remotes/origin/nightly
