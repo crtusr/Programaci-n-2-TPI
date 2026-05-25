c164389375ccd94a16dc610c2329c854c6fe5d8e
crtusr <lucasgasperi03@gmail.com>
2026-05-25 14:50:12 -0300

    Agregué clase Partida, intercala turnos y cuenta rondas

---
+++ partida.h
@@ -0,0 +1,17 @@
+#ifndef PARTIDA_H
+#define PARTIDA_H
+
+
+  class Partida
+  {
+    private:
+      int ronda;
+      int turno;
+    public:
+      Partida(int r = 0, int t = 0);
+      int getRonda();
+      int getTurno();
+      void pasarTurno(); 
+  };
+
+#endif
