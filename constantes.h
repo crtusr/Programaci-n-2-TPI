c5ec297664792fa7a08e713e392ce29c83ad0489
lucascwk-141 <lucascwk@hotmail.com>
2026-06-07 15:47:58 -0300

    Update constantes.h

--- constantes.h
+++ constantes.h
@@ -18,7 +18,9 @@ enum DIRECCIONES
   ABAJO,
   DERECHA,
   ENTER,
-  RETROCESO
+  RETROCESO,
+  SPACE,
+  F
 };
 
 //Recomendaría que los enums sean en MAYUSCULAS, por consistencia
@@ -28,7 +30,8 @@ enum EstadoJuego
     PersonajeSeleccionado,
     MENU_PRINCIPAL,
     MENU_INGAME,
-    ANIMACION_BLOQUEANTE
+    ANIMACION_BLOQUEANTE,
+    PREPARAR_ATAQUE
 };
 
 enum ACCION_PERSONAJE
