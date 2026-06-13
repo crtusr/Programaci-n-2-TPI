367b615a0594feca8616320d27dc22dbe801d90b
lucascwk-141 <lucascwk@hotmail.com>
2026-06-12 21:09:27 -0300

    clase nueva
    animacion.cpp y .h

---
+++ animacion.cpp
@@ -0,0 +1,45 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <filesystem>
+#include "animacion.h"
+#include "constantes.h"
+
+using namespace std;
+using namespace sf;
+
+void Animacion::asignaranimacion(vector<personaje>& pers,int* impactos,int* v_danios,int cant){
+int cont=0;
+
+for(int i=0; i<6; i++){
+  if(!anim_activa[i]&&cont<cant){
+   anim_activa[i]=true;
+   danios[i]=v_danios[cont];
+   v_posx[i]=pers[impactos[cont]].getPosxPxl();
+   v_posy[i]=pers[impactos[cont]].getPosyPxl();
+   cont++;
+  }
+}
+anim_frame=0;
+}
+void Animacion::mostraranimacion(RenderWindow& window){
+    int x;
+    int y;
+    if(anim_frame==0){x=0;y=0;}
+    if(anim_frame>10){x=3;y=0;}
+    if(anim_frame>20){x=4;y=0;}
+    if(anim_frame>30){x=5;y=0;}
+    if(anim_frame>40){x=6;y=0;}
+   sprite.setTextureRect(sf::IntRect(sf::Vector2i(64*x,64*y),sf::Vector2i(64,64)));
+
+for(int i=0; i<6; i++){
+  if(anim_activa[i]){
+sprite.setPosition(sf::Vector2f(v_posx[i],v_posy[i]));
+  window.draw(sprite);
+  if(anim_frame>60){anim_activa[i]=false;}
+    }
+}
+
+anim_frame++;
+
+}
+
---
+++ animacion.h
@@ -0,0 +1,37 @@
+#ifndef ANIMACION_H_INCLUDED
+#define ANIMACION_H_INCLUDED
+#include <SFML/Graphics.hpp>
+#include "personaje.h"
+
+using namespace std;
+using namespace sf;
+
+class Animacion{
+
+private:
+int anim_frame = 0;
+bool anim_activa[6] = {};
+int v_posx[6];
+int v_posy[6];
+int danios[6];
+Sprite sprite;
+Texture textura;
+public:
+        Animacion() : sprite(textura)
+    {
+        if (!textura.loadFromFile("imagen/Sprite-000_gplpe1.png")) {
+            std::cerr << "Error al cargar textura\n";
+        }
+    sprite.setTexture(textura);
+    sprite.setPosition(sf::Vector2f(0,0));
+    sprite.setTextureRect(sf::IntRect(
+        sf::Vector2i(64*0,64*0),      // posición inicial dentro del spritesheet---Vector2i:pertenece a la libreria de SFLM y contiene 2 vectores
+        sf::Vector2i(64, 64)         // tamaño del recorte
+    ));
+    }
+void asignaranimacion(vector<personaje>& pers,int* impactos,int* v_danios,int cant);
+void mostraranimacion(RenderWindow& window);
+
+};
+
+#endif // ANIMACION_H_INCLUDED
