bf111db4dd9dd49381d0da1c2aaa72a86330615c
lucascwk-141 <lucascwk@hotmail.com>
2026-06-21 00:37:25 -0300

    nuevos ataques

--- ataque.h
+++ ataque.h
@@ -16,10 +16,14 @@ private:
     int impactos[6];
     int cantidadimpactos = 0;
     int daniosimpactos[6];
+    int distancia = 0;
+    int tipodeataque;
 
 
     Sprite sprite;
     Sprite sprite2;
+    Sprite sprite3;
+    Sprite sprite4;
 
     sf::Texture textura;
 
@@ -39,18 +43,29 @@ public:
         posy=0;
         sprite.setTexture(textura);
         sprite2.setTexture(textura);
+        sprite3.setTexture(textura);
+        sprite4.setTexture(textura);
         sprite.setPosition(sf::Vector2f(posx,posy));
         sprite2.setPosition(sf::Vector2f(posx,posy));
+        sprite3.setPosition(sf::Vector2f(posx,posy));
+        sprite4.setPosition(sf::Vector2f(posx,posy));
 
 
         sprite.setTextureRect(sf::IntRect(
             sf::Vector2i(64*0,64*0),      // posición inicial dentro del spritesheet---Vector2i:pertenece a la libreria de SFLM y contiene 2 vectores
-            sf::Vector2i(64, 64)         // tamaño del recorte
-        ));
+            sf::Vector2i(64, 64)));        // tamaño del recorte
+
         sprite2.setTextureRect(sf::IntRect(
-            sf::Vector2i(64*1,64*0),      // posición inicial dentro del spritesheet---Vector2i:pertenece a la libreria de SFLM y contiene 2 vectores
-            sf::Vector2i(64, 64)         // tamaño del recorte
-        ));
+            sf::Vector2i(64*1,64*0),
+            sf::Vector2i(64, 64)));
+
+        sprite3.setTextureRect(sf::IntRect(
+            sf::Vector2i(64*2,64*0),
+            sf::Vector2i(64, 64)));
+
+        sprite4.setTextureRect(sf::IntRect(
+            sf::Vector2i(64*3,64*0),
+            sf::Vector2i(64, 64)));
     }
 
     // Getters
@@ -61,12 +76,15 @@ public:
     int getcantidadimpactos();
     int getimpacto(int pos);
     int getdanio(int pos);
+    int getdistancia();
+    int gettipodeataque();
     int getopciondeataque(){return opc;} // temporal asta que menu elija el ataque
 
 void ataque1(int direccion,RenderWindow& window,vector<personaje>& pers_atk,vector<personaje>& pers_def,managerpersonaje& manager);
 void ataque2(int direccion,RenderWindow& window,vector<personaje>& pers_atk,vector<personaje>& pers_def,managerpersonaje& manager);
-void ataque3();
-void ataque4();
+void ataque3(int direccion,RenderWindow& window,vector<personaje>& pers_atk,vector<personaje>& pers_def,managerpersonaje& manager);
+void ataque4(int direccion,RenderWindow& window,vector<personaje>& pers_atk,vector<personaje>& pers_def,managerpersonaje& manager);
+void curacion1(int direccion,RenderWindow& window,vector<personaje>& pers_atk,vector<personaje>& pers_def,managerpersonaje& manager);
 void prepararataque(int direccion,RenderWindow& window,vector<personaje>& pers_atk,vector<personaje>& pers_def,managerpersonaje& manager,int opc_atk);
 };
 #endif // ATAQUE_H_INCLUDED
